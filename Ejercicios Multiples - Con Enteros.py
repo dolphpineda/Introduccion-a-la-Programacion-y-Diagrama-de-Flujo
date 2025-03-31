a2b07663e2388577339b152dfb9f7cc195da4f79
dolphpineda <pineda0310@gmail.com>
2025-03-31 07:50:36 -0400

    commit

---
+++ Ejercicios Multiples - Con Enteros.py
@@ -0,0 +1,16 @@
+###Ejercicios multiples
+
+##1.1 - Ejercicios
+
+#variable llamada Edad y asignale el valor 25#
+edad=25 
+
+print (edad)
+
+
+##1.2 - Suma dos números enteros 15 y 30 y muestra el resultado
+
+numero1 = 15
+numero2 = 30
+suma = numero1 + numero2
+print(suma)  # Esto imprimirá 45 en la consola
